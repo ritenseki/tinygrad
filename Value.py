3c0634cd4642debfcabb1bd46bbc67d16f391834
ritenseki <ritenseki@gmail.com>
2026-04-09 20:43:56 +0800

    value.py, overrided 4 operator

---
+++ Value.py
@@ -0,0 +1,45 @@
+class Value:
+    def __init__(self, data, _children=()):
+        self.data = data;
+        self.grad = 0.0;
+        self._prev = set(_children)
+        self._backward = lambda: None
+    
+    def __repr__(self) -> str:
+        return f"Value(data={self.data}, grad={self.grad})"
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other))
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other))
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward()
